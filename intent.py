89ee961d261a3e76bde505d0a7a724af9f768558
rajsimran1996@gmail.com <Your Email Id>
2018-03-12 10:33:47 +0530

    intent classification with regex 1

---
+++ intent.py
@@ -0,0 +1,12 @@
+# Define a dictionary of patterns
+import re
+
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = re.compile('|'.join(keys))
+
+# Print the patterns
+print(patterns)
