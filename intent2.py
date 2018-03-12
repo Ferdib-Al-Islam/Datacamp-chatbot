e928c0e3ac9c0cfe0e6268a71a91f732b1356c1b
rajsimran1996@gmail.com <Your Email Id>
2018-03-12 10:50:24 +0530

    intent classification with regex 2

---
+++ intent2.py
@@ -0,0 +1,26 @@
+# Define a function to find the intent of a message
+from intent import patterns
+
+
+def match_intent(message):
+    matched_intent = None
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message
+        if pattern.search(message):
+            matched_intent = intent
+    return matched_intent
+
+# Define a respond function
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    return responses[key]
+
+# Send messages
+send_message("hello!")
+send_message("bye byeee")
+send_message("thanks very much!")
